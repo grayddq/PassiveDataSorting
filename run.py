347456803ca437b4ba467371334899568d2ecf4d
grayddq <280495355@qq.com>
2017-12-15 10:13:01 +0800

    更新代码

--- run.py
+++ run.py
@@ -2,10 +2,10 @@
 from lib.sniffer import *
 from lib.config import *
 
-NAME, VERSION, AUTHOR, LICENSE = "PublicDataSorting", "V0.1", "咚咚呛", "Public (FREE)"
+NAME, VERSION, AUTHOR, LICENSE = "PublicLogSorting", "V0.1", "咚咚呛", "Public (FREE)"
 
 if __name__ == "__main__":
     redis_r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD, db=REDIS_DB)
     if not 'conf_sniffer_rule' in redis_r.hkeys('passive_config'):
-        redis_r.hset('passive_config', 'conf_sniffer_rule',conf_sniffer_rule)
+        redis_r.hset('passive_config', 'conf_sniffer_rule', conf_sniffer_rule)
     Capute().run()
